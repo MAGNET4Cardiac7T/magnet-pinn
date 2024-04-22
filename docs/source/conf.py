b85cd0e48ac0014fca138004591a9aac50dda41f
Andrzej Dulny <andzej.dulny@gmail.com>
2024-04-22 21:56:06 +0200

    Changed theme to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,10 +26,24 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- Options for autodoc extension -------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#module-sphinx.ext.autodoc
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'special-members': False,
+}
+autosummary_imported_members = True
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_css_files = [
+    "dynabench.css",
+]
