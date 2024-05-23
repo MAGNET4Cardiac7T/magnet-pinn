eef8c22e1489382d6fda78d48d23755a16705089
Alex Tarasov <tarasov.oleksander@gmail.com>
2024-05-23 02:49:24 +0200

    Update reading field class for the new data realities.

--- magnet_pinn/preprocessing/reading_field.py
+++ magnet_pinn/preprocessing/reading_field.py
@@ -9,42 +9,17 @@ from h5py import File
 
 E_FIELD_DATABASE_KEY = "E-Field"
 H_FIELD_DATABASE_KEY = "H-Field"
-POSITIONS_DATABASE_KEY = "Position"
+X_BOUNDS_DATABASE_KEY = "Mesh line x"
+Y_BOUNDS_DATABASE_KEY = "Mesh line y"
+Z_BOUNDS_DATABASE_KEY = "Mesh line z"
 
 FIELD_DIR_PATH = {E_FIELD_DATABASE_KEY: "E_field", H_FIELD_DATABASE_KEY: "H_field"}
-
-ASCII_COLUMN_NAMES = {
-    E_FIELD_DATABASE_KEY: [
-        "x",
-        "y",
-        "z",
-        "ExRe",
-        "EyRe",
-        "EzRe",
-        "ExIm",
-        "EyIm",
-        "EzIm",
-    ],
-    H_FIELD_DATABASE_KEY: [
-        "x",
-        "y",
-        "z",
-        "HxRe",
-        "HyRe",
-        "HzRe",
-        "HxIm",
-        "HyIm",
-        "HzIm",
-    ],
-}
-
-ASCII_FILENAME_PATTERN = "*AC*.txt"
 H5_FILENAME_PATTERN = "*AC*.h5"
 
 
 class FieldReader:
     """Class for reading H/E field values.
-    We assume values can be only packed in .h5/.txt files"""
+    We assume values can be only packed in .h5 files"""
 
     def __init__(
         self, simulation_dir_path: str, field_type: str = E_FIELD_DATABASE_KEY
@@ -52,12 +27,13 @@ class FieldReader:
         self.field_type = field_type
 
         field_dir_path = osp.join(simulation_dir_path, FIELD_DIR_PATH[field_type])
-        self.files_list = list(
+        dir_files = list(
             map(
                 lambda x: osp.join(field_dir_path, x),
                 sorted(os.listdir(field_dir_path)),
             )
         )
+        self.files_list = fnmatch.filter(dir_files, H5_FILENAME_PATTERN)
 
         if len(self.files_list) == 0:
             raise Exception(
@@ -68,64 +44,43 @@ class FieldReader:
                 """
             )
 
-        if len(fnmatch.filter(self.files_list, H5_FILENAME_PATTERN)) != 0:
-            self.__coordinates_read_method__ = self.__read_coordinates_from_h5__
-            self.__data_read_method__ = self.__read_data_from_h5__
-        else:
-            self.__ascii_columns_order__ = self.__get_real_columns_order__()
-            self.__coordinates_read_method__ = self.__read__coordinates_from_ascii__
-            self.__data_read_method__ = self.__read_data_from_ascii__
+        self.x_bounds, self.y_bounds, self.z_bounds = self.__get_bounds__()
 
-        self.coordinates = self.__get__cordinates__()
+    def __get_bounds__(self):
+        x_bound, y_bound, z_bound = self.__read_bounds__(self.files_list[0])
+
+        for file_path in self.files_list[1:]:
+            other_x_bound, other_y_bound, other_z_bound = self.__read_bounds__(
+                file_path
+            )
+            if not (
+                np.array_equal(x_bound, other_x_bound)
+                and np.array_equal(y_bound, other_y_bound)
+                and np.array_equal(z_bound, other_z_bound)
+            ):
+                raise Exception(f"Different bounds in the field value file {file_path}")
+
+        return x_bound, y_bound, z_bound
+
+    def __read_bounds__(self, file_path: str):
+        with File(file_path) as f:
+            x_bounds = f[X_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+            y_bounds = f[Y_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+            z_bounds = f[Z_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+
+        return x_bounds, y_bounds, z_bounds
 
     def read_data(self):
-        return np.stack(list(map(self.__data_read_method__, self.files_list)), axis=-1)
+        field_values = list(map(self.__read_field_data__, self.files_list))
+        return np.stack(field_values, axis=-1)
 
-    def __read_data_from_h5__(self, file_path: str):
+    def __read_field_data__(self, file_path: str):
         with File(file_path) as f:
-            values = f.get(self.field_type)[:]
+            values = f[self.field_type][:]
 
         # Extract Ex,Ey,Ez as complex
         Ex = values["x"]["re"] + 1j * values["x"]["im"]
         Ey = values["y"]["re"] + 1j * values["y"]["im"]
         Ez = values["z"]["re"] + 1j * values["z"]["im"]
 
-        return np.column_stack((Ex, Ey, Ez)).astype(np.complex128)
-
-    def __get__cordinates__(self):
-        coordinates = self.__coordinates_read_method__(self.files_list[0])
-        for file_path in self.files_list[1:]:
-            other_coordinatess = self.__coordinates_read_method__(file_path)
-            if not np.array_equal(coordinates, other_coordinatess):
-                raise Exception(
-                    f"Different coordinates in the field value file {file_path}"
-                )
-        return coordinates
-
-    def __read_coordinates_from_h5__(self, file_path: str):
-        with File(file_path) as f:
-            positions = f[POSITIONS_DATABASE_KEY][:]
-
-        return np.column_stack((positions["x"], positions["y"], positions["z"])).astype(
-            np.int64
-        )
-
-    def __get_real_columns_order__(self):
-        header = np.loadtxt(self.files_list[0], max_rows=1, dtype=str)
-        header = list(header[::2])
-        columns_order = ASCII_COLUMN_NAMES[self.field_type]
-        return [header.index(col) for col in columns_order]
-
-    def __read__coordinates_from_ascii__(self, file_path: str):
-        data = np.loadtxt(file_path, skiprows=2)[:, self.__ascii_columns_order__]
-        return data[:, :3].astype(np.int64)
-
-    def __read_data_from_ascii__(self, file_path: str):
-        data = np.loadtxt(file_path, skiprows=2)
-        data = data[:, self.__ascii_columns_order__]
-
-        Ex = data[:, 3] + 1j * data[:, 6]
-        Ey = data[:, 4] + 1j * data[:, 7]
-        Ez = data[:, 5] + 1j * data[:, 8]
-
-        return np.column_stack((Ex, Ey, Ez)).astype(np.complex128)
+        return np.stack((Ex, Ey, Ez), axis=-1).astype(np.complex128)
