aaa590056bc0ebe66467b2cdecb12298c8b31693
Oleksandr Tarasov <tarasov.oleksander@gmail.com>
2024-06-10 12:29:22 +0200

    Split reading field class for grid and pointslist

--- magnet_pinn/preprocessing/reading_field.py
+++ magnet_pinn/preprocessing/reading_field.py
@@ -1,8 +1,9 @@
 """Module for reading field values"""
 
-import fnmatch
 import os
+import fnmatch
 import os.path as osp
+from abc import ABC, abstractmethod
 
 import numpy as np
 from h5py import File
@@ -18,10 +19,7 @@ FIELD_DIR_PATH = {E_FIELD_DATABASE_KEY: "E_field", H_FIELD_DATABASE_KEY: "H_fiel
 H5_FILENAME_PATTERN = "*AC*.h5"
 
 
-class FieldReader:
-    """Class for reading H/E field values.
-    We assume values can be only packed in .h5 files"""
-
+class FieldReaderFactory:
     def __init__(
         self, simulation_dir_path: str, field_type: str = E_FIELD_DATABASE_KEY
     ):
@@ -44,70 +42,52 @@ class FieldReader:
                 for the {field_type} field
                 """
             )
-        
-        self.is_grid = self.__is_grid()
-        self.positions = self.__extract_positions()
-        
+
+    def create_reader(self):
+        if self.__is_grid():
+            return GridReader(self.files_list, self.field_type)
+        return PointReader(self.files_list, self.field_type)
+
     def __is_grid(self):
         with File(self.files_list[0]) as f:
             database_keys = list(f.keys())
         
         return POSISTIONS_DATABASE_KEY not in database_keys
+
+
+class FieldReader(ABC):
+    """Class for reading H/E field values.
+    We assume values can be only packed in .h5 files"""
+
+    def __init__(self, files_list: list, field_type: str):
+        self.files_list = files_list
+        self.field_type = field_type
+
+    def extract_positions(self):
+        default_coordinates = self._read_coordinates(self.files_list[0])
+
+        for other_file in self.files_list[1:]:
+            other_coordinates = self._read_coordinates(other_file)
+            if not self._check_same_coordinates(default_coordinates, other_coordinates):
+                raise Exception(
+                    f"Different positions in the field value file {other_file}"
+                )
         
-    def __extract_positions(self):
-        if self.is_grid:
-            return self.__get_positions_by_bounds()
-        else:
-            return self.__get_positions()
+        return default_coordinates
 
-    def __get_positions_by_bounds(self):
-        x_bound, y_bound, z_bound = self.__read_bounds__(self.files_list[0])
+    @abstractmethod
+    def _read_coordinates(self, file_path: str):
+        pass
 
-        for file_path in self.files_list[1:]:
-            other_x_bound, other_y_bound, other_z_bound = self.__read_bounds__(
-                file_path
-            )
-            if not (
-                np.array_equal(x_bound, other_x_bound)
-                and np.array_equal(y_bound, other_y_bound)
-                and np.array_equal(z_bound, other_z_bound)
-            ):
-                raise Exception(f"Different bounds in the field value file {file_path}")
-            
-        xx, yy, zz = np.meshgrid(x_bound, y_bound, z_bound, indexing="ij")
-        grid = np.stack((xx, yy, zz), axis=-1)
-        positions = grid.reshape(-1, 3)
-        return positions
+    @abstractmethod
+    def _check_same_coordinates(self, default_coordinates, other_coordinates):
+        pass
 
-    def __read_bounds__(self, file_path: str):
-        with File(file_path) as f:
-            x_bounds = f[X_BOUNDS_DATABASE_KEY][:].astype(np.int64)
-            y_bounds = f[Y_BOUNDS_DATABASE_KEY][:].astype(np.int64)
-            z_bounds = f[Z_BOUNDS_DATABASE_KEY][:].astype(np.int64)
-
-        return x_bounds, y_bounds, z_bounds
-    
-    def __get_positions(self):
-        positions = self.__read_positions(self.files_list[0])
-        for file_path in self.files_list[1:]:
-            other_positions = self.__read_positions(file_path)
-            if not np.array_equal(positions, other_positions):
-                raise Exception(f"Different coordinates in the field value file {file_path}")
-        
-        return positions
-    
-    def __read_positions(self, file_path: str):
-        with File(file_path) as f:
-            x = f[POSISTIONS_DATABASE_KEY]["x"][:]
-            y = f[POSISTIONS_DATABASE_KEY]["y"][:]
-            z = f[POSISTIONS_DATABASE_KEY]["z"][:]
-        return np.column_stack((x, y, z)).astype(np.int64)
-
-    def read_data(self):
-        field_values = list(map(self.__read_field_data__, self.files_list))
+    def extract_data(self):
+        field_values = list(map(self.__read_field_data, self.files_list))
         return np.stack(field_values, axis=-1)
 
-    def __read_field_data__(self, file_path: str):
+    def __read_field_data(self, file_path: str):
         with File(file_path) as f:
             values = f[self.field_type][:]
 
@@ -117,7 +97,37 @@ class FieldReader:
         Ez = values["z"]["re"] + 1j * values["z"]["im"]
 
         values = np.stack((Ex, Ey, Ez), axis=-1).astype(np.complex128)
-        if self.is_grid:
-            values = values.reshape(-1, 3)
         
         return values
+
+
+class GridReader(FieldReader):
+    def _read_coordinates(self, file_path: str):
+        with File(file_path) as f:
+            x_bounds = f[X_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+            y_bounds = f[Y_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+            z_bounds = f[Z_BOUNDS_DATABASE_KEY][:].astype(np.int64)
+
+        return x_bounds, y_bounds, z_bounds
+    
+    def _check_same_coordinates(self, default_coordinates, other_coordinates):
+        x_default_bound, y_default_bound, z_default_bound = default_coordinates
+        x_other_bound, y_other_bound, z_other_bound = other_coordinates
+
+        return (
+            np.array_equal(x_default_bound, x_other_bound)
+            and np.array_equal(y_default_bound, y_other_bound)
+            and np.array_equal(z_default_bound, z_other_bound)
+            )
+
+
+class PointReader(FieldReader):
+    def _read_coordinates(self, file_path: str):
+        with File(file_path) as f:
+            x = f[POSISTIONS_DATABASE_KEY]["x"][:]
+            y = f[POSISTIONS_DATABASE_KEY]["y"][:]
+            z = f[POSISTIONS_DATABASE_KEY]["z"][:]
+        return np.column_stack((x, y, z)).astype(np.int64)
+    
+    def _check_same_coordinates(self, default_coordinates, other_coordinates):
+        return np.array_equal(default_coordinates, other_coordinates)
