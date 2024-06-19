1f1ab1e8c0861d841c44320a089d1afe37ef13b4
Andrzej Dulny <andzej.dulny@gmail.com>
2024-06-19 22:55:50 +0200

    Add MinMax Normalizer

---
+++ magnet_pinn/utils/__init__.py
@@ -0,0 +1,1 @@
+from ._input_normalization import InputMinMaxNormalizer
--- magnet_pinn/utils/_input_normalization.py
+++ magnet_pinn/utils/_input_normalization.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+import tqdm
 
 from abc import ABC, abstractmethod
 
@@ -21,10 +23,52 @@ class InputNormalizer(ABC, torch.nn.Module):
         raise NotImplementedError
     
     @abstractmethod
-    def _fit_params(self, dataset):
+    def fit_params(self, dataset):
         raise NotImplementedError   
     
     
 class InputMinMaxNormalizer(InputNormalizer):
     def _normalize(self, x):
-        return (x - self.x_min) / (self.x_max - self.x_min)
+        return (x - self.params['x_min']) / (self.params['x_max'] - self.params['x_min'])
+    
+    def _denormalize(self, x):
+        return x * (self.params['x_max'] - self.params['x_min']) + self.params['x_min']
+    
+    def fit_params(self, dataset):
+        x_min = np.inf
+        x_max = np.NINF
+
+        for batch in tqdm.tqdm(dataset):
+            x = batch['input']
+            print(x.shape)
+            x_min = np.minimum(x_min, x.min(axis=(0, 2, 3)))
+            x_max = np.maximum(x_max, x.max(axis=(0, 2, 3)))
+
+        
+        self.params = {'x_min': x_min, 'x_max': x_max}
+        return self.params
+    
+
+class InputStandardNormalizer(InputNormalizer):
+    def _normalize(self, x):
+        return (x - self.params['x_mean']) / self.params['x_std']
+    
+    def _denormalize(self, x):
+        return x * self.params['x_std'] + self.params['x_mean']
+    
+    def fit_params(self, dataset):
+        x_mean = 0
+        x_std = 0
+        n = 0
+
+        for batch in tqdm.tqdm(dataset):
+            x = batch['input']
+            n += x.shape[0]
+            x_mean += x.sum(axis=(0, 2, 3))
+            x_std += (x**2).sum(axis=(0, 2, 3))
+        
+        x_mean /= n
+        x_std = np.sqrt(x_std / n - x_mean**2)
+        
+        self.params = {'x_mean': x_mean, 'x_std': x_std}
+        return self.params
