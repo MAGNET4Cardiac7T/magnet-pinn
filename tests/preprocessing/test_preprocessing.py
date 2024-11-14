092fa26e50d15fcc29fb873854dd5b9dc937e0b8
Oleksandr Tarasov <tarasov.oleksander@gmail.com>
2024-11-14 15:59:48 +0100

    Add shifted support

--- tests/preprocessing/test_preprocessing.py
+++ tests/preprocessing/test_preprocessing.py
@@ -223,6 +223,38 @@ def test_grid_central_float_multiple_simulations_valid_preprocessing(raw_batch_d
         check_central_subject_mask(f)
 
 
+def test_grid_shifted_one_simulation_valid_preprocessing(raw_batch_dir_path, processed_batch_dir_path):
+    grid_preprocessor = GridPreprocessing(
+        raw_batch_dir_path,
+        processed_batch_dir_path,
+        field_dtype=np.complex64,
+        x_min=-4,
+        x_max=4,
+        y_min=-4,
+        y_max=4,
+        z_min=-4,
+        z_max=4, 
+        voxel_size=1
+    )
+    grid_preprocessor.process_simulations(
+        [SHIFTED_SPHERE_SIM_NAME]
+    )
+
+    case_name = f"grid_voxel_size_1_data_type_complex64"
+    out_dir = processed_batch_dir_path / case_name
+
+    out_simulations_dir = out_dir / PROCESSED_SIMULATIONS_DIR_PATH
+    assert out_simulations_dir.exists()
+
+    out_simulation_file = out_simulations_dir / TARGET_FILE_NAME.format(name=SHIFTED_SPHERE_SIM_NAME)
+    assert out_simulation_file.exists()
+
+    with File(out_simulation_file) as f:
+        list(f.keys()) == [E_FIELD_OUT_KEY, H_FIELD_OUT_KEY, FEATURES_OUT_KEY, SUBJECT_OUT_KEY]
+        check_shifted_subject_mask(f)
+        check_shifted_features(f)
+
+
 def check_complex_fields(f: File):
     expected_field = np.zeros((3, 9, 9, 9, 1), dtype=np.complex64)
 
@@ -261,9 +293,21 @@ def check_central_subject_mask(f: File):
     subject_mask = f[SUBJECT_OUT_KEY][:]
     assert subject_mask.shape == (9, 9, 9, 1)
     assert subject_mask.dtype == np.bool_
-    expected_subject_mask = np.zeros((9, 9), dtype=np.float64)
+    expected_subject_mask = np.zeros((9, 9), dtype=np.bool_)
     expected_subject_mask[3:6, 3:6] = 1
     assert np.equal(subject_mask[:, :, 3, 0], expected_subject_mask).all()
+    assert np.equal(subject_mask[:, :, 4, 0], expected_subject_mask).all()
+    assert np.equal(subject_mask[:, :, 5, 0], expected_subject_mask).all()
+
+def check_shifted_subject_mask(f: File):
+    subject_mask = f[SUBJECT_OUT_KEY][:]
+    assert subject_mask.shape == (9, 9, 9, 1)
+    assert subject_mask.dtype == np.bool_
+    expected_subject_mask = np.zeros((9, 9), dtype=np.bool_)
+    expected_subject_mask[4:7, 4:7] = 1
+    assert np.equal(subject_mask[:, :, 3, 0], expected_subject_mask).all()
+    assert np.equal(subject_mask[:, :, 4, 0], expected_subject_mask).all()
+    assert np.equal(subject_mask[:, :, 5, 0], expected_subject_mask).all()
 
 
 def check_central_features(f: File):
@@ -275,3 +319,21 @@ def check_central_features(f: File):
     expected_features[3:6, :] = 1
     expected_features[6:9, 3:6] = 1
     assert np.equal(features[0, :, :, 3], expected_features).all()
+
+
+def check_shifted_features(f: File):
+    features = f[FEATURES_OUT_KEY][:]
+    assert features.shape == (3, 9, 9, 9)
+    assert features.dtype == np.float32
+    expected_features = np.zeros((9, 9), dtype=np.float32)
+    expected_features = np.zeros((9, 9), dtype=np.float32)
+    expected_features[0:3, 3:6] = 1
+    expected_features[3:6, 0:3] = 1
+    expected_features[3:6, 6:9] = 1
+    expected_features[4:7, 4:7] = 1
+    expected_features[6:9, 3:6] = 1
+    expected_features[6, 4:6] = 2
+    expected_features[4:6, 6] = 2
+    assert np.equal(features[0, :, :, 3], expected_features).all()
+    assert np.equal(features[0, :, :, 4], expected_features).all()
+    assert np.equal(features[0, :, :, 5], expected_features).all()
