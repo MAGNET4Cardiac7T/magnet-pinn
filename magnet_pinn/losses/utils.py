05cf43697f3923337638e7e9a1f4be55dce4c0bf
Andrzej Dulny <andzej.dulny@gmail.com>
2024-12-03 14:58:00 +0100

    Remove physics components from cherry pick

--- magnet_pinn/losses/utils.py
+++ magnet_pinn/losses/utils.py
@@ -13,15 +13,3 @@ class MaskedLossReducer(torch.nn.Module):
             if mask.shape != loss.shape:
                 raise ValueError(f"mask shape {mask.shape} does not match loss shape {loss.shape}")
         return torch.mean(loss[mask])
-    
-def mask_padding(input_shape_mask: torch.Tensor, padding: int = 1) -> torch.Tensor:
-    padding_filter = torch.ones([1,1] + [padding*2 + 1]*3, dtype=torch.float32)
-    check_border = torch.nn.functional.conv3d(input_shape_mask.type(torch.float32), padding_filter, padding=padding)
-    return check_border == torch.sum(padding_filter)
-    
-# TODO Calculate findiffs at different accuracies
-def partial_derivative_findiff(accuracy: int = 2) -> List[float]:
-    if accuracy == 2:
-        return [-0.5, 0, 0.5]
-    else:
-        raise ValueError(f"accuracy {accuracy} not supported")
