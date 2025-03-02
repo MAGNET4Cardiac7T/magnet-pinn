8f31ec626e9a58819058b7d0b4b51b2368308103
Alex Tarasov <oleksander.tarasov@arrk-engineering.com>
2025-03-02 06:59:33 +0100

    Add pointcloud cli tests and fixed "== None"

--- tests/preprocessing/test_cli.py
+++ tests/preprocessing/test_cli.py
@@ -25,7 +25,7 @@ def test_cli_check_grid_default_command(monkeypatch):
     assert args.preprocessing_type == "grid"
 
 
-def test_grid_cli_check_no_command(monkeypatch):
+def test_cli_check_no_command(monkeypatch):
     """
     Case when no command is given
     """
@@ -239,7 +239,7 @@ def test_grid_cli_check_simulations_default_value(monkeypatch):
         ]
     )
     args = parse_arguments()
-    assert args.simulations == None
+    assert args.simulations is None
 
 
 def test_grid_cli_check_voxel_size_given_valid_value(monkeypatch):
@@ -569,3 +569,221 @@ def test_grid_cli_check_default_z_max_value(monkeypatch):
     )
     args = parse_arguments()
     assert args.z_max == Z_MAX
+
+
+def test_cli_check_pointcloud_default_command(monkeypatch):
+    """
+    General check for the pointcloud command
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.preprocessing_type == "pointcloud"
+
+
+def test_pointcloud_cli_check_batches_argmunets_for_one_value(monkeypatch, raw_central_batch_dir_path):
+    """
+    Case when we pass a pointcloud command and one batch directory
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--batches", str(raw_central_batch_dir_path)
+        ]
+    )
+    args = parse_arguments()
+    assert args.batches == [raw_central_batch_dir_path]
+
+
+def test_pointcloud_cli_check_batches_argmunets_for_multiple_values(monkeypatch, raw_central_batch_dir_path, raw_shifted_batch_dir_path):
+    """
+    Case when we pass a pointcloud command and multiple batch directories
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--batches", str(raw_shifted_batch_dir_path), str(raw_central_batch_dir_path)
+        ]
+    )
+    args = parse_arguments()
+    assert args.batches == [raw_shifted_batch_dir_path, raw_central_batch_dir_path]
+
+
+def test_pointcloud_cli_check_batches_arguments_default_value(monkeypatch):
+    """
+    Case when we pass a pointcloud command and no batch directories
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.batches == BATCHES
+
+
+def test_pointcloud_cli_check_given_antenna_value(monkeypatch, raw_antenna_dir_path):
+    """
+    Case when we give an antenna argument
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--antenna", str(raw_antenna_dir_path)
+        ]
+    )
+    args = parse_arguments()
+    assert args.antenna == raw_antenna_dir_path
+
+
+def test_pointcloud_cli_check_default_antenna_value(monkeypatch):
+    """
+    Case when we check a default antenna value
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.antenna == ANTENNA_DIR
+
+
+def test_pointcloud_cli_check_given_output_value(monkeypatch, processed_batch_dir_path):
+    """
+    Case when we give an output argument
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--output", str(processed_batch_dir_path)
+        ]
+    )
+    args = parse_arguments()
+    assert args.output == processed_batch_dir_path
+
+
+def test_pointcloud_cli_check_default_output_value(monkeypatch):
+    """
+    Case when we check a default output value
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.output == OUTPUT_DIR
+
+
+def test_pointcloud_cli_check_given_field_dtype_valid_value(monkeypatch):
+    """
+    Case when we give a field_dtype argument
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--field_dtype", "float64"
+        ]
+    )
+    args = parse_arguments()
+    assert args.field_dtype == np.float64
+
+
+def test_pointcloud_cli_check_given_field_dtype_invalid_value(monkeypatch):
+    """
+    Case when we give an invalid field_dtype argument
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--field_dtype", "some_value"
+        ]
+    )
+    with pytest.raises(SystemExit):
+        parse_arguments()
+
+
+def test_pointcloud_cli_check_default_field_dtype_value(monkeypatch):
+    """
+    Case when we check a default field_dtype value
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.field_dtype == FIEld_DTYPE
+
+
+def test_pointcloud_cli_check_simulations_one_given_value(monkeypatch):
+    """
+    Case when we give a simulations argument
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--simulations", "simulation_1"
+        ]
+    )
+    args = parse_arguments()
+    assert args.simulations == [Path("simulation_1")]
+
+
+def test_pointcloud_cli_check_simulations_multiple_given_values(monkeypatch):
+    """
+    Case when we give multiple simulations arguments
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud",
+            "--simulations", "simulation_1", "simulation_2", "simulation_3"
+        ]
+    )
+    args = parse_arguments()
+    assert args.simulations == [Path("simulation_1"), Path("simulation_2"), Path("simulation_3")]
+
+
+def test_pointcloud_cli_check_simulations_default_value(monkeypatch):
+    """
+    Case when we check a default simulations value
+    """
+    monkeypatch.setattr(
+        "sys.argv", 
+        [
+            "script.py",
+            "pointcloud"
+        ]
+    )
+    args = parse_arguments()
+    assert args.simulations is None
