f23dd4287eb9720c6634428115acf28b2f6db798
Alex Tarasov <oleksander.tarasov@arrk-engineering.com>
2025-05-12 13:50:53 +0200

    Add a simple properties sampler.

---
+++ magnet_pinn/generator/properties.py
@@ -0,0 +1,19 @@
+"""
+NAME
+    properties.py
+DESCRIPTION
+    This module contains functions to generate physical properties for a mesh
+"""
+from typing import List
+
+import numpy as np
+
+
+class PropertySampler:
+    def __init__(self, properties_cfg):
+        self.properties_cfg = properties_cfg
+
+    def sample(self, properties_list: List = None):
+        if properties_list is None:
+            properties_list = list(self.properties_cfg.keys())
+        return {key: np.random.uniform(dim[0], dim[1]) for key, dim in self.properties_cfg.items() if key in properties_list}
