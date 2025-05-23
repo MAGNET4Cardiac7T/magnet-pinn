0f230762ad69d42167875cfdd7f97c615689d438
Alex Tarasov <oleksandr.tarasov@stud-mail.uni-wuerzburg.de>
2025-05-23 10:11:06 +0200

    Add postprocessing fixes.

--- magnet_pinn/generator/postprocessing.py
+++ magnet_pinn/generator/postprocessing.py
@@ -9,22 +9,24 @@ class Postprocessing(ABC):
         raise NotImplementedError("Subclasses should implement this method.")
 
 
-class TrimeshPostprocessing(Postprocessing):
+class MeshPostprocessing(Postprocessing):
     def process(self, parent: Trimesh, children: list[Trimesh], tubes: list[Trimesh]):
         cutters = children + tubes
-        union_cutters = trimesh.boolean.union(cutters, engine='blender')
-        parent_cut = trimesh.boolean.difference([parent, union_cutters], engine='blender')
+        union_cutters = trimesh.boolean.union(cutters, engine='manifold')
+        parent_cut = trimesh.boolean.difference([parent, union_cutters], engine='manifold')
+
         if not tubes:
             children_cut = children.copy()
         else:
             children_cut = []
-            tubes_union = trimesh.boolean.union(tubes, engine='blender')
+            tubes_union = trimesh.boolean.union(tubes, engine='manifold')
             for c in children:
                 children_cut.append(
-                    trimesh.boolean.difference([c, tubes_union], engine='blender')
+                    trimesh.boolean.difference([c, tubes_union], engine='manifold')
                 )
+         
         tubes_cut = [
-            trimesh.boolean.intersection([t, parent], engine='blender')
+            trimesh.boolean.intersection([t, parent], engine='manifold')
             for t in tubes
         ]
         
@@ -35,7 +37,8 @@ class TrimeshPostprocessing(Postprocessing):
         )
 
     def _remesh(self, mesh: Trimesh, max_len=0.8):
-        mesh = subdivide_to_size(mesh, max_len)
+        vertices, faces = subdivide_to_size(mesh.vertices, mesh.faces, max_len)
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
         mesh.remove_degenerate_faces()
         mesh.remove_unreferenced_vertices()
         return mesh
