c57f8374d5e0fb065d8385e4378364172625e7a9
Alex Tarasov <oleksandr.tarasov@stud-mail.uni-wuerzburg.de>
2025-05-27 11:53:55 +0200

    Fix usage of the seed and gave an example with the seed 42, which is deterministic.

--- magnet_pinn/generator/phantoms.py
+++ magnet_pinn/generator/phantoms.py
@@ -8,6 +8,7 @@ import logging
 from abc import ABC
 
 import numpy as np
+from numpy.random import Generator, default_rng
 
 from .structures import Blob, Tube
 from .typing import PhantomItem
@@ -15,14 +16,11 @@ from .typing import PhantomItem
 
 class Phantom(ABC):
     initial_blob_radius: float
-    initial_blob_center: np.ndarray
-    seed: int
+    initial_blob_center_extent: np.ndarray
 
-    def __init__(self, initial_blob_radius: float, initial_blob_center: np.ndarray):
+    def __init__(self, initial_blob_radius: float, initial_blob_center_extent: np.ndarray):
         self.initial_blob_radius = initial_blob_radius
-        
-        initial_blob_center = [np.random.uniform(dim[0], dim[1]) for key, dim in initial_blob_center.items()]
-        self.initial_blob_center = np.array(initial_blob_center)
+        self.initial_blob_center_extent = initial_blob_center_extent
 
     def generate(self, seed: int = None) -> PhantomItem:
         raise NotImplementedError("Subclasses should implement this method.")
@@ -38,12 +36,12 @@ class Tissue(Phantom):
     def __init__(self, 
                  num_children_blobs: int, 
                  initial_blob_radius: float, 
-                 initial_blob_center: np.ndarray,
+                 initial_blob_center_extent: np.ndarray,
                  blob_radius_decrease_per_level: float, 
                  num_tubes: int, relative_tube_max_radius: float,
                  relative_tube_min_radius: float = 0.01):
         
-        super().__init__(initial_blob_radius, initial_blob_center)
+        super().__init__(initial_blob_radius, initial_blob_center_extent)
 
         self.num_children_blobs = num_children_blobs
         
@@ -56,36 +54,42 @@ class Tissue(Phantom):
         self.relative_disruption_strength = 0.1
 
     def generate(self, seed: int = None) -> PhantomItem:
-                
+        gen_object = default_rng(seed)
+
+        initial_blob_center = np.array([
+            gen_object.uniform(dim[0], dim[1])
+            for _, dim in self.initial_blob_center_extent.items()
+        ])
+
         logging.info("Generating parent blob.")
-        self.parent_blob = Blob(self.initial_blob_center, self.initial_blob_radius, seed=None)
+        parent_blob = Blob(initial_blob_center, self.initial_blob_radius, seed=gen_object.uniform())
         
         logging.info("Generating children blobs.")
-        self.children_blobs = self._generate_children_blobs(parent_blob=self.parent_blob)
-        
+        children_blobs = self._generate_children_blobs(parent_blob=parent_blob, gen_object=gen_object)
+
         logging.info("Generating tubes.")
-        parent_inner_radius = self.parent_blob.radius*(1+self.parent_blob.empirical_min_offset)
-        self.tubes = self._sample_tubes_within_ball(self.initial_blob_center, parent_inner_radius, self.num_tubes, self.tube_max_radius, self.tube_min_radius)
+        parent_inner_radius = parent_blob.radius*(1+parent_blob.empirical_min_offset)
+        tubes = self._sample_tubes_within_ball(initial_blob_center, parent_inner_radius, self.num_tubes, self.tube_max_radius, self.tube_min_radius, gen_object=gen_object)
 
         return PhantomItem(
-            parent=self.parent_blob,
-            children=self.children_blobs,
-            tubes=self.tubes
+            parent=parent_blob,
+            children=children_blobs,
+            tubes=tubes
         )
         
-    def _generate_children_blobs(self, parent_blob: Blob = None, max_iterations: int = 10000000):
+    def _generate_children_blobs(self, parent_blob: Blob = None, max_iterations: int = 10000000, gen_object: Generator = None) -> list[Blob]:
         """
         Generate one hierarchy level.
         """
         if parent_blob is None:
-            return [Blob(self.initial_blob_center, self.initial_blob_radius)]
+            return [Blob(self.initial_blob_center, self.initial_blob_radius, seed=gen_object.uniform())]
         
         if self.num_children_blobs == 0:
             return []
         
         child_radius = parent_blob.radius*self.blob_radius_decrease_per_level
         zero_center = np.zeros_like(parent_blob.position)
-        blobs = [Blob(zero_center, child_radius, seed=None) for _ in range(self.num_children_blobs)]
+        blobs = [Blob(zero_center, child_radius, seed=gen_object.uniform()) for _ in range(self.num_children_blobs)]
         
         min_offset_children = np.min([blob.empirical_min_offset for blob in blobs])
         max_offset_children = np.max([blob.empirical_max_offset for blob in blobs])
@@ -107,7 +111,7 @@ class Tissue(Phantom):
         idx = 0
         logging.info("Finding suitable points for child blobs. Packing ratio: {:.3f}".format(child_radius_with_safe_margin/parent_inner_radius))
         while True:
-            points = self._sample_points_within_ball(parent_blob.position, parent_allowed_radius, num_points=self.num_children_blobs)
+            points = self._sample_points_within_ball(parent_blob.position, parent_allowed_radius, num_points=self.num_children_blobs, gen_object=gen_object)
 
             # check if all points are at least child_radius_with_safe_margin away from each other
             if self._check_points_are_at_least_distance_away(points, 2*child_radius_with_safe_margin):
@@ -153,45 +157,45 @@ class Tissue(Phantom):
         if np.any(min_distances < min_distance):
             return False
         return True
-    
-    def _sample_points_within_ball(self, center: np.ndarray, radius: float, num_points: int = 1):
+
+    def _sample_points_within_ball(self, center: np.ndarray, radius: float, num_points: int = 1, gen_object: Generator = None):
         """
         Sample multiple points within a ball uniformly.
         """
-        points = np.random.normal(size=(num_points, len(center)))
-        
+        points = gen_object.normal(size=(num_points, len(center)))
+
         points = points / np.linalg.norm(points, axis=1)[:,None]
-        points = points * radius* np.random.uniform(0, 1, size=(num_points,1))**(1/len(center))
+        points = points * radius* gen_object.uniform(0, 1, size=(num_points,1))**(1/len(center))
         points = points + center
         return points
-    
-    def _sample_point_within_ball(self, center: np.ndarray, radius: float):
+
+    def _sample_point_within_ball(self, center: np.ndarray, radius: float, gen_object: Generator = None):
         """
         Sample a point within a ball.
         """
         # Sample a point on the surface of the ball.
-        point = np.random.normal(size=center.shape)
+        point = gen_object.normal(size=center.shape)
         point = radius*point / np.linalg.norm(point)
         
         # Sample a point within the ball.
-        point = np.random.uniform(0, 1) ** (1 / len(center)) * point
+        point = gen_object.uniform(0, 1) ** (1 / len(center)) * point
         return point+center
 
-    def _sample_line_within_ball(self, center: np.ndarray, ball_radius: float, tube_radius: float):
+    def _sample_line_within_ball(self, center: np.ndarray, ball_radius: float, tube_radius: float, gen_object: Generator = None) -> Tube:
         """
         Sample a line within a ball.
         """
         # Sample a point on the surface of the ball.
-        point = self._sample_point_within_ball(center, ball_radius)
-        
+        point = self._sample_point_within_ball(center, ball_radius, gen_object=gen_object)
+
         # Sample a direction that is perpendicular to the radius from the center to the point.
-        direction = np.random.normal(size=center.shape)
+        direction = gen_object.normal(size=center.shape)
         direction = direction - np.dot(direction, point - center) / np.linalg.norm(point - center) ** 2 * (point - center)
         direction = direction / np.linalg.norm(direction)
         return Tube(point, direction, tube_radius)
 
 
-    def _sample_tubes_within_ball(self, center: np.ndarray, radius: float, num_tubes: int, tube_max_radius: float, tube_min_radius: float = 0.01):
+    def _sample_tubes_within_ball(self, center: np.ndarray, radius: float, num_tubes: int, tube_max_radius: float, tube_min_radius: float = 0.01, gen_object: Generator = None) -> list[Tube]:
         """
         Sample tubes within a ball.
         """
@@ -200,8 +204,8 @@ class Tissue(Phantom):
         for i in range(num_tubes):
             while True:
                 # Sample a tube.
-                tube_radius = np.random.uniform(tube_min_radius, tube_max_radius)
-                tube = self._sample_line_within_ball(center, radius-tube_radius, tube_radius)
+                tube_radius = gen_object.uniform(tube_min_radius, tube_max_radius)
+                tube = self._sample_line_within_ball(center, radius-tube_radius, tube_radius, gen_object=gen_object)
 
 
                 # check if tube completely within the ball.
