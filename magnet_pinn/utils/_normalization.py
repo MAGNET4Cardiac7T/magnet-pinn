Fix tests for normalization (but not all of them)

magnet_pinn/utils/_normalization.py
@@ -143,10 +143,11 @@ class Normalizer(torch.nn.Module):
         if not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
         with open(path, 'w') as f:
+            params = self._params.copy()
             # add nonlinearity info
-            self._params['nonlinearity'] = self.nonlinearity_name
-            self._params['nonlinearity_before'] = self.nonlinearity_before
-            json.dump(self._params, f)
+            params['nonlinearity'] = self.nonlinearity_name
+            params['nonlinearity_before'] = self.nonlinearity_before
+            json.dump(params, f)
 
     def _get_nonlineartiy_function(name: str = "Identity"):
         if name == 'Identity':

tests/utils/test_normalization.py
@@ -103,10 +103,10 @@ def test_save_and_load_minmax_normalizer(tmp_path, random_iterator):
     
     assert original_params == loaded_params, "Loaded parameters do not match the original parameters"
 
-def test_save_and_load_standard_normalizer(tmp_path):
+def test_save_and_load_standard_normalizer(tmp_path, random_iterator):
     normalizer = StandardNormalizer()
-    data = torch.tensor([[1.0, 2.0], [3.0, 4.0]])
-    normalizer.fit_params([{"input": data}])
+    iterator = random_iterator(seed=42, num_batches=10, batch_size=10, num_features=3)
+    normalizer.fit_params(iterator, axis=1)
     
     original_params = normalizer.params.copy()  # Save original parameters
     save_path = tmp_path / "standard_normalizer.json"