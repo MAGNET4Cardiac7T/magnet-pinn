5d8fec272bb5a5d3463af808ffc710d5a2f4d69e
Andrzej Dulny <andzej.dulny@gmail.com>
2025-10-10 12:12:56 +0200

    Import metanormalizer in __init__

--- magnet_pinn/utils/__init__.py
+++ magnet_pinn/utils/__init__.py
@@ -2,7 +2,8 @@
     A module containing the Normalization utilities.
 """
 
-from ._normalization import StandardNormalizer, MinMaxNormalizer, Identity, Log, Power, Tanh, Arcsinh
+from ._normalization import StandardNormalizer, MinMaxNormalizer, MetaNormalizer
+from ._normalization import Identity, Log, Power, Tanh, Arcsinh
 from ._perlin_noise import PerlinNoise
 
 __all__ = [
