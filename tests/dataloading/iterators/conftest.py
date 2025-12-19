78236c6211be497dd6676d7de887d59a538ce953
Alex Tarasov <oleksandr.tarasov@stud-mail.uni-wuerzburg.de>
2025-12-19 11:34:11 +0100

    make `tests/dataloading/iterators` aligned with the linters.

--- tests/dataloading/iterators/conftest.py
+++ tests/dataloading/iterators/conftest.py
@@ -1,15 +1,11 @@
+"""Pytest fixtures for iterator tests."""
+
 from shutil import rmtree
 
 import pytest
-import numpy as np
-from h5py import File
 
-from magnet_pinn.data.transforms import (
-    Compose, PhaseShift, PointPhaseShift
-)
-from tests.dataloading.iterators.helpers import (
-    create_processed_dir
-)
+from magnet_pinn.data.transforms import PhaseShift, PointPhaseShift
+from tests.dataloading.iterators.helpers import create_processed_dir
 
 
 GRID_RPOCESSED_DIR_NAME = "test_grid_voxel_size_4_data_type_float32"
@@ -20,6 +16,7 @@ POINTCLOUD_PREPROCESSED_DIR_NAME_SHORT_TERM = "test_point_data_type_float32_shor
 
 @pytest.fixture(scope='module')
 def grid_processed_dir(processed_dir_path, random_grid_item, zero_grid_item):
+    """Create a module-scoped grid processed directory with test data."""
     grid_processed_dir_path = processed_dir_path / GRID_RPOCESSED_DIR_NAME
     create_processed_dir(grid_processed_dir_path, random_grid_item, zero_grid_item, is_grid=True)
     yield grid_processed_dir_path
@@ -29,6 +26,7 @@ def grid_processed_dir(processed_dir_path, random_grid_item, zero_grid_item):
 
 @pytest.fixture(scope='function')
 def grid_prodcessed_dir_short_term(processed_dir_path, random_grid_item, zero_grid_item):
+    """Create a function-scoped grid processed directory for short-term tests."""
     grid_processed_dir_path = processed_dir_path / GRID_PROCESSED_DIR_NAME_SHORT_TERM
     create_processed_dir(grid_processed_dir_path, random_grid_item, zero_grid_item, is_grid=True)
     yield grid_processed_dir_path
@@ -38,6 +36,7 @@ def grid_prodcessed_dir_short_term(processed_dir_path, random_grid_item, zero_gr
 
 @pytest.fixture(scope='module')
 def pointcloud_processed_dir(processed_dir_path, random_pointcloud_item, zero_pointcloud_item):
+    """Create a module-scoped pointcloud processed directory with test data."""
     pointcloud_processed_dir_path = processed_dir_path / POINTCLOUD_PREPROCESSED_DIR_NAME
     create_processed_dir(pointcloud_processed_dir_path, random_pointcloud_item, zero_pointcloud_item, is_grid=False)
     yield pointcloud_processed_dir_path
@@ -47,6 +46,7 @@ def pointcloud_processed_dir(processed_dir_path, random_pointcloud_item, zero_po
 
 @pytest.fixture(scope='function')
 def pointcloud_processed_dir_short_term(processed_dir_path, random_pointcloud_item, zero_pointcloud_item):
+    """Create a function-scoped pointcloud processed directory for short-term tests."""
     pointcloud_processed_dir_path = processed_dir_path / POINTCLOUD_PREPROCESSED_DIR_NAME_SHORT_TERM
     create_processed_dir(pointcloud_processed_dir_path, random_pointcloud_item, zero_pointcloud_item, is_grid=False)
     yield pointcloud_processed_dir_path
@@ -56,9 +56,11 @@ def pointcloud_processed_dir_short_term(processed_dir_path, random_pointcloud_it
 
 @pytest.fixture(scope='module')
 def grid_aug():
+    """Create a PhaseShift augmentation for grid data tests."""
     return PhaseShift(num_coils=8)
 
 
 @pytest.fixture(scope='module')
 def pointcloud_aug():
+    """Create a PointPhaseShift augmentation for pointcloud data tests."""
     return PointPhaseShift(num_coils=8)
