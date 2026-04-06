aff4deb4c215c74209a40d28575186f5527cac3c
JavaStudentAlex <tarasov.oleksander@gmail.com>
2026-04-06 15:05:21 +0200

    fix(losses): added code for the losses tests

--- magnet_pinn/losses/__init__.py
+++ magnet_pinn/losses/__init__.py
@@ -4,7 +4,7 @@ Physics informed losses and constraints for the magnet_pinn package.
 
 from .base import MSELoss, MAELoss, HuberLoss, LogCoshLoss
 from .physics import BasePhysicsLoss, DivergenceLoss, FaradaysLawLoss, MRI_FREQUENCY_HZ, VACUUM_PERMEABILITY
-from .utils import mask_padding
+from .utils import mask_padding, ResidualNorm
 
 __all__ = [
     'MSELoss',
@@ -17,4 +17,5 @@ __all__ = [
     'MRI_FREQUENCY_HZ',
     'VACUUM_PERMEABILITY',
     'mask_padding',
+    'ResidualNorm',
 ]
